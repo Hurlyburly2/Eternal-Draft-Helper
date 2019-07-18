c2ff7b586ed3ff6123fd4ddd34d3c24175149765
Hurlyburly2 <dougdougmann@gmail.com>
2019-07-18 14:39:56 -0400

    javascript file uploader complete!

--- app/javascript/react/containers/FormTestContainer.js
+++ app/javascript/react/containers/FormTestContainer.js
@@ -10,9 +10,51 @@ class FormTestContainer extends Component {
     this.handleSubmit = this.handleSubmit.bind(this)
   }
   
+  
+  
+  // const results = document.querySelector("#results");
+  
   handleSubmit(event) {
-    debugger
     event.preventDefault()
+    const form = document.querySelector("#my_form");
+    const formData = new FormData(form);
+    
+    fetch('/api/v1/screenshots', {
+      method: "POST",
+      body: formData,
+      credentials: 'same-origin'
+    });
+    
+    
+      // Select your input type file and store it in a variable
+      // const input = document.getElementById('image_upload');
+      // 
+      // debugger
+      // 
+      // // This will upload the file after having read it
+      // const upload = (file) => {
+      //   fetch('/api/v1/screenshots', { // Your POST endpoint
+      //     method: 'POST',
+      //     // headers: {
+      //     //   // Content-Type may need to be completely **omitted**
+      //     //   // or you may need something
+      //     // },
+      //     body: file // This is your file object
+      //   }).then(
+      //     response => response.json() // if the response is a JSON object
+      //   ).then(
+      //     success => console.log(success) // Handle the success response object
+      //   ).catch(
+      //     error => console.log(error) // Handle the error response object
+      //   );
+      // };
+      // 
+      // // Event handler executed when a file is selected
+      // const onSelectFile = () => upload(input.files[0]);
+      // 
+      // // Add a listener on your input
+      // // It will be triggered when a file will be selected
+      // input.addEventListener('change', onSelectFile, false);
   }
   
   render() {
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   
   resources :homes, only: [:index, :create]
   resources :screenshots, only: [:create]
+  
+  namespace :api do
+    namespace :v1 do
+      resources :screenshots, only: [:index, :create]
+    end
+  end
 end
