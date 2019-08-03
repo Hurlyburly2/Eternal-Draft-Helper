0ffcb275808952e797f8732730cdec6bd88d6839
Hurlyburly2 <dougdougmann@gmail.com>
2019-08-03 05:39:49 -0400

    commented out config/initializers/devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'homes#index'
-  devise_for :users
+  # devise_for :users
   
   resources :homes, only: [:index, :create]
   resources :screenshots, only: [:create]
